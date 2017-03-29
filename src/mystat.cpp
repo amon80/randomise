e39809714f8ba31d397bb3a7a33a5eecd6e60a42
Marco Mecchia <m.mecchia@studenti.unisa.it>
2017-03-29 11:28:15 +0200

    Added Tstatistic

--- src/mystat.cpp
+++ src/mystat.cpp
@@ -1,12 +1,33 @@
 #include "mystat.h"
+#include "mymath.h"
+#include <cmath>
+
+
+//usable only when s=rank(C)==1
+float TStatistic(Eigen::VectorXd& phi, Eigen::VectorXd& epsilon, Eigen::MatrixXd& M, Eigen::MatrixXd& C, int s, std::vector<int>& VGS){
+    float FValue = FStatistic(phi, epsilon, M, C, s, VGS);
+    float beta = phi(0);
+    return sgn(beta)*sqrt(FValue);
+}
 
 float FStatistic(Eigen::VectorXd& phi, Eigen::VectorXd& epsilon, Eigen::MatrixXd& M, Eigen::MatrixXd& C, int s, std::vector<int>& VGS){
     Eigen::MatrixXd Ctrasp = C.transpose();
-    //M's rank is it's number of columns
-    int rankM = M.cols();
+    //M's rank is it's number of columns since the design matrix is full rank
+    int r = M.cols();
+    int rankM = r;
+    int rankZ = r - s;
     int N = M.rows();
-    Eigen::MatrixXd denominatorMatrix = (epsilon.transpose()*epsilon)/(N - rankM);
-    Eigen::MatrixXd numeratorMatrix = phi.transpose()*C*(Ctrasp*((M.transpose()*M).inverse())*C)*Ctrasp*phi;
+    Eigen::MatrixXd X = Eigen::MatrixXd::Zero(N, s);
+    Eigen::VectorXd beta = Eigen::VectorXd::Zero(s);
+    for(int i = 0; i < s; i++){
+        for(int j = 0; j < N; j++){
+            X(j,i) = M(j,i);
+        }
+    }
+    for(int i = 0; i < s; i++)
+        beta(i) = phi(i);
+    Eigen::MatrixXd denominatorMatrix = (epsilon.transpose()*epsilon)/(N - rankM - rankZ);
+    Eigen::MatrixXd numeratorMatrix = beta.transpose()*(X.transpose()*X)*beta;
     float denominator = denominatorMatrix(0,0);
     float numerator = numeratorMatrix(0,0);
     numerator /= s;
--- src/mystat.h
+++ src/mystat.h
@@ -4,6 +4,7 @@
 #include <Eigen/Dense>
 #include <vector>
 
+float TStatistic(Eigen::VectorXd& phi, Eigen::VectorXd& epsilon, Eigen::MatrixXd& M, Eigen::MatrixXd& C, int s, std::vector<int>& VGS);
 float FStatistic(Eigen::VectorXd& phi, Eigen::VectorXd& epsilon, Eigen::MatrixXd& M, Eigen::MatrixXd &C, int s, std::vector<int>& VGS);
 float GStatistic(Eigen::VectorXd& phi, Eigen::VectorXd& epsilon, Eigen::MatrixXd& M, Eigen::MatrixXd &C, int s, std::vector<int>& VGS);
 
