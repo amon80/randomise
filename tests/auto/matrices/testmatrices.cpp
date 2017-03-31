e8f4ba7294cc9963a575b1a35cddf1d7508a284d
Marco Mecchia <m.mecchia@studenti.unisa.it>
2017-03-31 14:00:46 +0200

    Added test prototype for matrices

--- tests/auto/matrices/testmatrices.cpp
+++ tests/auto/matrices/testmatrices.cpp
@@ -10,33 +10,20 @@ public:
     TestMatrices();
     ~TestMatrices();
 
-private:
-    Eigen::MatrixXd M1;
-    Eigen::MatrixXd C1;
-    Eigen::MatrixXd C2;
-
 private slots:
     void initTestCase();
     void partitionModelTestCase1();
+    void partitionModelTestCase2();
+    void partitionModelTestCase3();
+    void partitionModelTestCase4();
+    void partitionModelTestCase5();
+    void example5noNuisance();
     void cleanUpTestCase();
 };
 
 TestMatrices::TestMatrices()
-    : M1(Eigen::MatrixXd (3, 2)),
-      C1(Eigen::MatrixXd (2, 1)),
-      C2(Eigen::MatrixXd (1, 1))
 {
-    for(int i = 0; i < 3; i++){
-        M1(i,0) = 1;
-    }
-    for(int i = 0; i < 3; i++){
-        M1(i,1) = i+10;
-    }
 
-    C1(0,0) = 1;
-    C1(1,0) = 0;
-
-    C2(0,0) = 1;
 }
 
 TestMatrices::~TestMatrices()
@@ -50,11 +37,52 @@ void TestMatrices::initTestCase(){
 
 }
 
+//No nuisance, one sample Ttest
+//TODO
 void TestMatrices::partitionModelTestCase1(){
-    PartitioningResult result = partitionModel(M1, C1);
-    std::cout << result.X << std::endl;
-    std::cout << result.Z << std::endl;
+
 }
+
+//No nuisance, two sample unpaired Ttest
+//TODO
+void TestMatrices::partitionModelTestCase2(){
+
+}
+
+//Nuisance, two sample unpaired Ttest
+//TODO
+void TestMatrices::partitionModelTestCase3(){
+
+}
+
+//Nuisance, two sample unpaired Ttest
+//TODO
+void TestMatrices::partitionModelTestCase4(){
+
+}
+
+//wo-Sample Paired T-test (Paired Two-Group Difference)
+void TestMatrices::partitionModelTestCase5(){
+
+}
+
+void TestMatrices::example5noNuisance(){
+    Eigen::MatrixXd M = Eigen::MatrixXd::Zero(4,2);
+
+    M(0,0) = 1;
+    M(1,0) = 1;
+    M(2,1) = 1;
+    M(3,1) = 1;
+
+    Eigen::MatrixXd C(2,1);
+
+    C(0,0) = 1;
+    C(1,0) = -1;
+
+    PartitioningResult p = partitionModel(M, C);
+
+}
+
 
 void TestMatrices::cleanUpTestCase(){
 
